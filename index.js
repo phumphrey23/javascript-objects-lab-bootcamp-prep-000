3dda9cc11d5b3234c72f0673828cf2f697ca50b1
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-16 13:24:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {};
 var obj = {prop: 1};
 function updateObjectWithKeyAndValue(object, key, value){
-  object.assign({},obj,{[key]: value });
-  return obj;
+  object.assign({},object,{[key]: value });
+  return object;
 };
 updateObjectWithKeyAndValue(obj, 'prop2', 2);
 
