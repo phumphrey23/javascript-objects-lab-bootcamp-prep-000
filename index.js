04f307d1faea55186bedee259bb2131b1ec9893d
Precious Humphrey <phumphrey23@yahoo.com>
2018-01-16 13:34:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value;
   return object;
 };
-  var newObj = Object.assign({}, object);
+
 function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
   delete newObj[key];
   return newObj;
 };
